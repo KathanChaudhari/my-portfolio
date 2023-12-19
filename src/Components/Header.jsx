fd99590d14b3de8dd26e790aa5988b8f0030aae3
kathan chaudhari <kathanc2651@gmail.com>
2023-12-19 12:22:07 +0530

    check 5

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -20,20 +20,21 @@ function Header() {
     }
 
     return (
-        <div className={`w-full p-4 bg-[#263238] border-4 border-black z-50`}>
-                <div className={`w-full justify-end px-10 flex space-x-16 mt-2  text-blue-600 ${banger.className}`}>
-                    <span className="cursor-pointer" onClick={(e) => handleLinkClick(e)}><Link href="/">Home</Link></span>
-                    <span className="cursor-pointer" onClick={(e) => handleLinkClick(e)}><Link href="/aboutme">About Me</Link></span>
-                    <span className="cursor-pointer" onClick={(e) => handleLinkClick(e)}><Link href="/projects">Projects</Link></span>
-                    <span className="cursor-pointer" onClick={(e) => handleLinkClick(e)}><Link href="/skills">Skills</Link></span>
-                    <span className="cursor-pointer" onClick={(e) => handleLinkClick(e)}><Link href="/blogs">Blogs</Link></span>
-            </div>
-            {clicked &&
-                <div className="absolute z-50 p-2 bg-white rounded-full" style={{ left: position.x , top: position.y }}>
-                    <p className={`text-black font-bold text-sm ${banger.className} `}>CLICKED!</p>
-                </div>
-            }
+        <div style={{ padding: '16px', backgroundColor: '#263238', border: '4px solid black', zIndex: 50 }}>
+    <div className={`${banger.className}`} style={{ width: '100%', justifyContent: 'flex-end', padding: '0 40px', display: 'flex', marginTop: '8px', color: '#007bff' } }>
+        <span style={{ cursor: 'pointer', marginRight: '24px' }} onClick={(e) => handleLinkClick(e)}><Link href="/">Home</Link></span>
+        <span style={{ cursor: 'pointer', marginRight: '24px' }} onClick={(e) => handleLinkClick(e)}><Link href="/aboutme">About Me</Link></span>
+        <span style={{ cursor: 'pointer', marginRight: '24px' }} onClick={(e) => handleLinkClick(e)}><Link href="/projects">Projects</Link></span>
+        <span style={{ cursor: 'pointer', marginRight: '24px' }} onClick={(e) => handleLinkClick(e)}><Link href="/skills">Skills</Link></span>
+        <span style={{ cursor: 'pointer', marginRight: '24px' }} onClick={(e) => handleLinkClick(e)}><Link href="/blogs">Blogs</Link></span>
+    </div>
+    {clicked &&
+        <div style={{ position: 'absolute', zIndex: 50, padding: '8px', backgroundColor: 'white', borderRadius: '9999px', left: position.x, top: position.y }}>
+            <p style={{ color: 'black', fontWeight: 'bold', fontSize: '14px' }}>CLICKED!</p>
         </div>
+    }
+</div>
+
     );
 }
 
