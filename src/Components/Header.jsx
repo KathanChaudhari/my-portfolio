d094f6184a777370b4ea44f63001339a5a0c5c89
kathan chaudhari <kathanc2651@gmail.com>
2023-12-19 11:55:06 +0530

    check 2

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -21,7 +21,7 @@ function Header() {
 
     return (
         <div className={` sticky p-4 bg-[#263238] border-4 border-black z-10`}>
-            <nav className="z-20 flex justify-end mx-10">
+            <div className="z-20 flex justify-end mx-10">
                 <div className={`flex space-x-16 mt-2 bg-opacity-20 text-blue-600 ${banger.className}`}>
                     <span className="cursor-pointer" onClick={(e) => handleLinkClick(e)}><Link href="/">Home</Link></span>
                     <span className="cursor-pointer" onClick={(e) => handleLinkClick(e)}><Link href="/aboutme">About Me</Link></span>
@@ -29,7 +29,7 @@ function Header() {
                     <span className="cursor-pointer" onClick={(e) => handleLinkClick(e)}><Link href="/skills">Skills</Link></span>
                     <span className="cursor-pointer" onClick={(e) => handleLinkClick(e)}><Link href="/blogs">Blogs</Link></span>
                 </div>
-            </nav>
+            </div>
             {clicked &&
                 <div className="absolute z-50 p-2 bg-white rounded-full" style={{ left: position.x , top: position.y }}>
                     <p className={`text-black font-bold text-sm ${banger.className} `}>CLICKED!</p>
